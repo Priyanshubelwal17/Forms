d97cd6df39b79c827d9a400861b8ad5c6c1a3ebf
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-02-01 14:31:34 +0530

    Add controlled input for username

---
+++ src/UsernameForm.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function UsernameForm() {
+    const [username, setUsername] = useState("timmy")
+    const updateusername = (evt) => {
+        console.log("Change Event!!")
+        setUsername(evt.target.value)
+    }
+    return (
+        <div>
+            <input onChange={updateusername} type="text" placeholder="username" value={username} />
+            <button>Submit</button>
+        </div>
+    )
+}
+
+export default UsernameForm;
